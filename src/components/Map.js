0ae41d659d5b6c5783633baea172311683c893c6
theochampion <contact.theochampion@gmail.com>
2018-09-04 17:14:06 +1000

    :sparkles: Render mapbox basic map

---
+++ src/components/Map.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
+
+export default class Map extends Component {
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v9"
+    });
+  }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
+  render() {
+    const style = {
+      position: "absolute",
+      top: 0,
+      bottom: 0,
+      width: "100%"
+    };
+
+    return <div style={style} ref={el => (this.mapContainer = el)} />;
+  }
+}
